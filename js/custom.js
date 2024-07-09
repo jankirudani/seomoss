c7120fd15918983df2b6c3ca427ce20843e86ca8
jankirudani <rudanijanki47@gmail.com>
2024-07-09 12:06:16 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function openNav() {
+    jQuery("#mySidenav").css("width", "300px");
+    }
+
+    // jQuery version for closeNav
+    function closeNav() {
+    jQuery("#mySidenav").css("width", "0");
+    }
