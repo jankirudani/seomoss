36d53dcb94db2c177ae5111911e1f92c74088de1
jankirudani <rudanijanki47@gmail.com>
2024-07-09 20:17:00 +0530

    add header-footer-deal section code 9-7-24

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,13 @@
+
+
 function openNav() {
     jQuery("#mySidenav").css("width", "300px");
-    }
+}
 
-    // jQuery version for closeNav
-    function closeNav() {
+//close
+function closeNav() {
     jQuery("#mySidenav").css("width", "0");
-    }
+}
+
+
+
