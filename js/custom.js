402f1e968f3fc87267abf01af20d67150782a0fd
jankirudani <rudanijanki47@gmail.com>
2024-07-10 20:13:22 +0530

    add about deal gallery section code 10-7-24

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,53 @@
+jQuery( document ).ready(function() {
+    //Banner slider js
+    jQuery('.client-logo').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots:true,
+        autoplaySpeed: 2000
+    });
 
+    //Mobile-menu js
+    function openNav() {
+        jQuery("#mySidenav").css("width", "300px");
+    }
+    function closeNav() {
+        jQuery("#mySidenav").css("width", "0");
+    }
+    });
 
-function openNav() {
-    jQuery("#mySidenav").css("width", "300px");
-}
+    //dealday slider js
+    function initializeSlider() {
+        if ($(window).width() <= 1024) {
+            if (!$('.dealday__cards').hasClass('slick-initialized')) {
+                $('.dealday__cards').slick({
+                    dots: true,
+                    arrows:false,
+                    infinite: true,
+                    speed: 300,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    responsive: [
+                        {
+                            breakpoint: 767,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]
+                });
+            }
+        } else {
+            if ($('.dealday__cards').hasClass('slick-initialized')) {
+                $('.dealday__cards').slick('unslick');
+            }
+        }
+    }
 
-//close
-function closeNav() {
-    jQuery("#mySidenav").css("width", "0");
-}
-
+    // Initialize slider on load
+    initializeSlider();
 
 
