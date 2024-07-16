ae59a7b152c7af28b6c71b045c220d7e091c7897
jankirudani <rudanijanki47@gmail.com>
2024-07-16 19:53:45 +0530

    custom-js changes 16-7-24

--- js/custom.js
+++ js/custom.js
@@ -1,99 +1,93 @@
 jQuery( document ).ready(function() {
     //Banner slider js
     jQuery('.banner-sliders').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots:true,
-        arrows:false,
-        autoplaySpeed: 2000
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots:true,
+      arrows:false,
+      autoplaySpeed: 2000
     });
 
     //testimonial slider js
     jQuery('.testimonial__data').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows:true,
-        autoplaySpeed: 2000,
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev-review"></button>',
-        nextArrow: '<button type="button" class="slick-next-review"></button>'
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows:true,
+      autoplaySpeed: 2000,
+      dots: true,
+      prevArrow: '<button type="button" class="slick-prev-review"></button>',
+      nextArrow: '<button type="button" class="slick-next-review"></button>'
     });
 
     //dealday slider js
     function initializeSlider() {
-        if (jQuery(window).width() <= 1024) {
-            if (!jQuery('.dealday__cards').hasClass('slick-initialized')) {
-                jQuery('.dealday__cards').slick({
-                    dots: true,
-                    arrows:false,
-                    infinite: true,
-                    speed: 300,
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    responsive: [
-                        {
-                            breakpoint: 767,
-                            settings: {
-                                slidesToShow: 1,
-                                slidesToScroll: 1
-                            }
-                        }
-                    ]
-                });
-            }
-        } else {
-            if (jQuery('.dealday__cards').hasClass('slick-initialized')) {
-                jQuery('.dealday__cards').slick('unslick');
-            }
+      if (jQuery(window).width() <= 1024) {
+          if (!jQuery('.dealday__cards').hasClass('slick-initialized')) {
+              jQuery('.dealday__cards').slick({
+                  dots: true,
+                  arrows:false,
+                  infinite: true,
+                  speed: 300,
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                  responsive: [
+                      {
+                          breakpoint: 767,
+                          settings: {
+                              slidesToShow: 1,
+                              slidesToScroll: 1
+                          }
+                      }
+                  ]
+              });
+          }
+      } else {
+        if (jQuery('.dealday__cards').hasClass('slick-initialized')) {
+            jQuery('.dealday__cards').slick('unslick');
         }
+      }
     }
-
-    // Initialize slider on load
     initializeSlider();
-
 
     //product tabbing with slider
     $(document).ready(function() {
-        $('.tab-links a').on('click', function(e) {
-          var currentAttrValue = $(this).attr('href');
-    
-          // Show/Hide Tabs
-          $('.tab-content').hide();
-          $(currentAttrValue).show();
-    
-          // Change/remove current tab to active
-          $('.tab-links li').removeClass('active');
-          $(this).parent('li').addClass('active');
-    
-          e.preventDefault();
-        });
+      $('.tab-links a').on('click', function(e) {
+        var currentAttrValue = $(this).attr('href');
+  
+        $('.tab-content').hide();
+        $(currentAttrValue).show();
+  
+        $('.tab-links li').removeClass('active');
+        $(this).parent('li').addClass('active');
+  
+        e.preventDefault();
+      });
     });
 
     $('.dealday__cards-tab').slick({
-        rows: 2,
-        slidesToShow: 3,  
-        slidesToScroll: 3,  
-        dots: true,  
-        arrows: false,
-        infinite: true,  
-        responsive: [
-          {
-            breakpoint: 1024,  
-            settings: {
-              slidesToShow: 2,  
-              slidesToScroll: 2,  
-            }
-          },
-          {
-            breakpoint: 767,  
-            settings: {
-              slidesToShow: 1,  
-              slidesToScroll: 1,  
-            }
+      rows: 2,
+      slidesToShow: 3,  
+      slidesToScroll: 3,  
+      dots: true,  
+      arrows: false,
+      infinite: true,  
+      responsive: [
+        {
+          breakpoint: 1024,  
+          settings: {
+            slidesToShow: 2,  
+            slidesToScroll: 2,  
           }
-        ]
-      });
-
+        },
+        {
+          breakpoint: 767,  
+          settings: {
+            slidesToShow: 1,  
+            slidesToScroll: 1,  
+          }
+        }
+      ]
+    });
 });
