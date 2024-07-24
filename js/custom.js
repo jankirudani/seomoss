2ec5c4e57463961652137a3826a04c9b6455adae
jankirudani <rudanijanki47@gmail.com>
2024-07-24 20:04:48 +0530

    feedback changes 24-7-24

--- js/custom.js
+++ js/custom.js
@@ -1,94 +1,94 @@
 jQuery( document ).ready(function() {
 
-   //Mobile-menu js
-   function openNav() {
+  //Mobile-menu js
+  function openNav() {
     $("#mySidenav").css("width", "250px");
-}
-
-function closeNav() {
+  }
+  function closeNav() {
     $("#mySidenav").css("width", "0");
-}
+  }
 
 
-  //gallery section
-    const items = $(".animate-swap-tabs__item");
-    function switchTab(index) {
-        let current_Item = $(items[index]).attr("data-item");
-        let left_translateY = $(items[index]).attr("data-ly");
-        let right_translateY = $(items[index]).attr("data-ry");
+  // Gallery section
+  const items = $(".animate-swap-tabs__item");
 
-        $(items[index]).addClass("animate-swap-tabs__item--active");
-        $(items[index]).siblings().removeClass("animate-swap-tabs__item--active");
+  function switchTab(index) {
+    let current_Item = $(items[index]).attr("data-item");
+    let left_translateY = $(items[index]).attr("data-ly");
+    let right_translateY = $(items[index]).attr("data-ry");
 
-        setTimeout(() => {
-            $(".animate-swap-tabs__screen-image-box." + current_Item).css({
-                visibility: "visible",
-                opacity: "1",
-            });
-            $(".animate-swap-tabs__object-image-box." + current_Item).css({
-                opacity: "1",
-                transform: "translateY(0) scale(1) translateZ(0px)",
-            });
-            $(".animate-swap-tabs__screen-image-box." + current_Item)
-                .siblings()
-                .css({
-                    opacity: "0",
-                });
-        }, 200);
+    $(items[index]).addClass("animate-swap-tabs__item--active");
+    $(items[index]).siblings().removeClass("animate-swap-tabs__item--active");
 
-        // Animate the transformation of siblings
-        $(".animate-swap-tabs__object-image-box")
-            .not("." + current_Item)
-            .css({
-                opacity: "0",
-                transform: "translateY(-75px) scale(0.8) translateZ(0px)",
-            });
+    setTimeout(() => {
+      $(".animate-swap-tabs__screen-image-box." + current_Item).css({
+          visibility: "visible",
+          opacity: "1",
+      });
+      $(".animate-swap-tabs__object-image-box." + current_Item).css({
+          opacity: "1",
+          transform: "translateY(0) scale(1) translateZ(0px)",
+      });
+      $(".animate-swap-tabs__screen-image-box." + current_Item)
+          .siblings()
+          .css({
+              opacity: "0",
+          });
+    }, 200);
 
-        $(".animate-swap-tabs__left-shape").css(
-            "transform",
-            "translateY(" + left_translateY + ") translateZ(0px)"
-        );
-        $(".animate-swap-tabs__right-shape").css(
-            "transform",
-            "translateY(" + right_translateY + ") translateZ(0px)"
-        );
-    }
-    items.hover(function () {
-        const currentIndex = $(this).index();
-        switchTab(currentIndex);
-    });
+    // Animate the transformation of siblings
+    $(".animate-swap-tabs__object-image-box")
+      .not("." + current_Item)
+      .css({
+          opacity: "0",
+          transform: "translateY(-75px) scale(0.8) translateZ(0px)",
+      });
 
-  //Gallery sticky
-    jQuery('li .gallery__info-box').hover(
-      function() {
-          jQuery('li .gallery__info-box').removeClass('pop');
-          jQuery(this).addClass('pop');
-      },
-      function() {
-      }
+    $(".animate-swap-tabs__left-shape").css(
+      "transform",
+      "translateY(" + left_translateY + ") translateZ(0px)"
     );
+    $(".animate-swap-tabs__right-shape").css(
+      "transform",
+      "translateY(" + right_translateY + ") translateZ(0px)"
+    );
+  }
+
+  items.on("click", function () {
+    const currentIndex = $(this).index();
+    switchTab(currentIndex);
+  });
+
+  // Gallery sticky
+  $('li .gallery__info-box').on("click", function() {
+    $('li .gallery__info-box').removeClass('pop');
+    $(this).addClass('pop');
+  });
 
   //Banner slider js
   jQuery('.banner-sliders').slick({
+    dots: true, 
+    arrows: false,
     infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots:true,
-    arrows:false,
-    autoplaySpeed: 2000
+    autoplay: false,
+    autoplaySpeed: 5000,
+    pauseOnHover: true,
+    slidesToShow: 1
   });
 
   //testimonial slider js
   jQuery('.testimonial__data').slick({
+    dots: true, 
+    arrows: true,
     infinite: true,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    pauseOnHover: true,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows:true,
-    autoplaySpeed: 2000,
-    dots: true,
     prevArrow: '<button type="button" class="slick-prev-review"></button>',
     nextArrow: '<button type="button" class="slick-next-review"></button>'
   });
+  
 
   //dealday slider js
   function initializeSlider() {
@@ -96,7 +96,7 @@ function closeNav() {
       if (!jQuery('.dealday__cards').hasClass('slick-initialized')) {
         jQuery('.dealday__cards').slick({
           dots: true,
-          arrows:false,
+          arrows: false,
           infinite: true,
           speed: 300,
           slidesToShow: 2,
@@ -105,8 +105,8 @@ function closeNav() {
             {
               breakpoint: 767,
               settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
+                slidesToShow: 1,
+                dots: true
               }
             }
           ]
@@ -114,11 +114,19 @@ function closeNav() {
       }
     } else {
       if (jQuery('.dealday__cards').hasClass('slick-initialized')) {
-          jQuery('.dealday__cards').slick('unslick');
+        jQuery('.dealday__cards').slick('unslick');
       }
     }
   }
-  initializeSlider();
+
+  jQuery(document).ready(function() {
+    initializeSlider();
+  });
+
+  jQuery(window).resize(function() {
+    initializeSlider();
+  });
+
 
 
 
@@ -134,41 +142,41 @@ function closeNav() {
     slidesToScroll: 3,
     infinite: false,
     responsive: [
-        {
-            breakpoint: 1300,
-            settings: {
-                rows: 2,
-                slidesToScroll: 1,
-                slidesToShow: 2,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-                rows: 2,
-                slidesToScroll: 2,
-                slidesToShow: 2,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                rows: 2,
-                slidesToScroll: 1,
-                slidesToShow: 1,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToScroll: 1,
-                slidesToShow: 1,
-                dots: true
-            }
+      {
+        breakpoint: 1300,
+        settings: {
+            rows: 2,
+            slidesToScroll: 1,
+            slidesToShow: 2,
+            dots: true
         }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+            rows: 2,
+            slidesToScroll: 2,
+            slidesToShow: 2,
+            dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+            rows: 2,
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            dots: true
+        }
+      }
     ]
   };
 
@@ -184,20 +192,20 @@ function closeNav() {
     this.item = item;
 
     this.filterFunc = function() {
-        $('.active').removeClass('active');
-        $('.js-filter[data-filter="'+this.item+'"]').addClass('active');
-        var filterSlides = allSlides.filter(this.item);
+      $('.active').removeClass('active');
+      $('.js-filter[data-filter="'+this.item+'"]').addClass('active');
+      var filterSlides = allSlides.filter(this.item);
 
-        var slider = $('.slider2');
-        slider.css({"opacity": "0", "left": "50px"});
-        setTimeout(function() {
-            slider
-                .slick('unslick')
-                .empty()
-                .append(filterSlides)
-                .slick(slickoptions)
-                .css({"opacity": "100", "left": "0px"});
-        }, 600);
+      var slider = $('.slider2');
+      slider.css({"opacity": "0", "left": "50px"});
+      setTimeout(function() {
+        slider
+          .slick('unslick')
+          .empty()
+          .append(filterSlides)
+          .slick(slickoptions)
+          .css({"opacity": "100", "left": "0px"});
+      }, 600);
     };
   };
 
@@ -207,6 +215,5 @@ function closeNav() {
     var newFilter = new ClassFilter(this, attr);
     newFilter.filterFunc();
   });
-
 
 });
